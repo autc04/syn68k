6c72dac2e0fb2f286c5af56abcbc4bdc1c9931ff
C.W. Betts <computers57@hotmail.com>
2013-07-11 10:07:09 -0600

    Make autogen.sh work on Darwin/OS X by checking for glibtool/glibtoolize first. The libtool binary that OS X uses is actually not the same version that other unices use. The version that other unices use is named glibtool on OS X. It seems that syn68k uses arguments that aren't available on OS X's/Darwin's libtool.

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,14 @@
 
 set -o errexit -o nounset -o noclobber
 
-libtoolize
+test -z "$LIBTOOL" && for LIBTOOL in glibtool libtool; do
+  ($LIBTOOL --version) < /dev/null > /dev/null 2>&1 && break
+done
+test -z "$LIBTOOLIZE" && for LIBTOOLIZE in glibtoolize libtoolize; do
+  ($LIBTOOLIZE --version) < /dev/null > /dev/null 2>&1 && break
+done
+
+$LIBTOOLIZE
 aclocal
 autoheader
 automake --add-missing
