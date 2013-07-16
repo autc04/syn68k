ec57c0a0996c593ac43355ef9ab2a182faeee02e
C.W. Betts <computers57@hotmail.com>
2013-07-15 19:12:06 -0600

    Get the glibtool/libtool checker in autogen.sh to work on an actual system.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
 # $Id: autogen.sh 65 2004-12-24 19:51:17Z ctm $
 
-set -o errexit -o nounset -o noclobber
+#set -o errexit -o nounset -o noclobber
 
 test -z "$LIBTOOL" && for LIBTOOL in glibtool libtool; do
   ($LIBTOOL --version) < /dev/null > /dev/null 2>&1 && break
